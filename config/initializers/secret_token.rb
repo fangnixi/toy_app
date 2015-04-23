18f5eb1e66728d682ca4b369dc7309fb2ca44760
Fangni Xi <fangnixi@seas.upenn.edu>
2015-04-22 21:08:00 -0400

    Initilize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = '3b8c23b2a23a7d5c68ac8dc4616ce7ff71b2e773c641601aacc92f72bfc869cd5e062b39b7cc7f5125267b7f29be319998fbdebf95af69df82d79f147387e77a'
