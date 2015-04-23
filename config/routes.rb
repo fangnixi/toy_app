30b1647a31261a7f72232d11c85b1647f0cedca2
Fangni Xi <fangnixi@seas.upenn.edu>
2015-04-22 23:01:32 -0400

    Finish toy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ToyApp::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+  root :to => 'users#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
